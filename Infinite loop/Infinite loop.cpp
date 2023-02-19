7bb8f1fccadf2e3f526a3a93e068f8cad0421406
TonyDo23 <123401343+TonyDo23@users.noreply.github.com>
2023-02-19 00:00:38 -0600

    Add files via upload

---
+++ Infinite loop/Infinite loop.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	string choice = "Y";
+
+	//same type "string'
+	string name, occup, age;
+	int part;
+
+	while (choice == "Y") {
+
+		cout << "1: Enter Name\n";
+		// Add input name
+		cin >> name;
+
+		cout << "2: Enter Age\n";
+		// Add input age
+		cin >> age;
+
+		cout << "3: Enter Occupation\n";
+		// Add input occup
+		cin >> occup;
+
+
+		//The user may not know input -1 to end the loop
+		//Change to Y or N for easier to understand
+		while (choice == "Y") {
+
+			// ask if the user want to change any info
+			// let user choose Y or N
+			//Y for changing info
+			//N for end the loop, output
+			cout << "\nDo you want to change your info? (Y or N)\n";
+			cin >> choice;
+
+			//make sure user choose Y or N
+			while (choice != "Y" && choice != "N") {
+				cout << "\nPlease inter Y or N\n";
+				cin >> choice;
+			}
+
+			//User want to change info
+			if (choice == "Y") {
+				//Ask which part they want to change
+				//input part
+				cout << "\nWhich part you want to change? (Enter 1 for changing name, 2 for changing age, 3 for changing occupation)\n";
+				cin >> part;
+
+				// for user change what is wrong
+				switch (part)
+				{
+				case 1:
+					cout << "What is your Name: ";
+					cin >> name;
+					break;
+				case 2:
+					cout << "What is your Age? ";
+					cin >> age;
+			
+					break;
+				case 3:
+					cout << "What is your Occupation? ";
+					cin >> occup;
+					break;
+				default:
+					// Assume Invalid Menu Choice
+					cout << "Sorry that choice is not valid!\n";
+					break;
+				}
+			} 
+			//chosing N end the loop
+			//output
+			else if (choice == "N") {
+				age += 1;
+				cout << "\nThank you for using our application, " << name << " and hope your career in " << occup << " is successful. ";
+				cout << "Hope your " << age << " birthday will enjoyable and exciting\n";
+			}
+		}
+
+	}
+
+	return 0;
+
+}
